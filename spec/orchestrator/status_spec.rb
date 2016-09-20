d28c7db2b10022c7b4e0c32b371877eee7bcd40f
Stephen von Takach <steve@cotag.me>
2016-09-20 10:47:39 +1000

    (status:spec) added tests for subscriptions

--- spec/orchestrator/status_spec.rb
+++ spec/orchestrator/status_spec.rb
@@ -4,10 +4,11 @@ require 'orchestrator'
 class MockController
     def initialize(log)
         @log = log
+        @loaded = {}
     end
 
     def loaded?(mod_id)
-
+        @loaded[mod_id]
     end
 
     def log_unhandled_exception(e)
@@ -15,6 +16,7 @@ class MockController
     end
 
     attr_reader :log
+    attr_accessor :loaded
 end
 
 class MockModule
@@ -27,6 +29,24 @@ class MockModule
 
     attr_reader   :status
     attr_accessor :settings, :thread
+end
+
+class MockSysProxy
+    def initialize
+        @modules = {}
+        @id = "sys_1-#{rand(10..9999)}"
+    end
+
+    attr_reader :id
+
+    def get(mod_name, index)
+        @modules[mod_name.to_sym][index - 1]
+    end
+
+    def add_module(mock_module, mod_name)
+        @modules[mod_name.to_sym] ||= []
+        @modules[mod_name.to_sym] << mock_module
+    end
 end
 
 describe Orchestrator::Subscription do
@@ -77,7 +97,6 @@ describe Orchestrator::Subscription do
 end
 
 
-# subscribe + unsubscribe
 # move + transfer
 # system reload
 describe Orchestrator::Status do
@@ -86,19 +105,29 @@ describe Orchestrator::Status do
         @controller = MockController.new(@log)
         @status = Orchestrator::Status.new(reactor, @controller)
         @mod = MockModule.new
+
+        @reactor = reactor
+        status = @status
+        @reactor.instance_eval { @observer = status }
     end
 
 
-    describe 'module subscription' do
+    describe 'direct module subscriptions' do
         before :each do
-            @sub = @status.subscribe(status: :testing, callback: proc { |sub| @log << sub.value }, on_thread: reactor, mod: @mod, mod_id: @mod.settings.id.to_sym)
+            @sub = @status.subscribe(
+                status: :testing,
+                callback: proc { |sub| @log << sub.value },
+                on_thread: reactor,
+                mod: @mod,
+                mod_id: @mod.settings.id.to_sym
+            )
         end
 
-        it "should subscribe to modules directly" do
+        it "should subscribe to module status directly" do
             expect(@log).to eq([])
             expect(@sub.value).to be(nil)
 
-            reactor.run { |reactor|
+            @reactor.run { |reactor|
                 @status.update(@sub.mod_id, @sub.status, 'what what')
                 expect(@sub.value).to eq('what what')
                 expect(@log).to eq(['what what'])
@@ -110,8 +139,118 @@ describe Orchestrator::Status do
                 @status.update(@sub.mod_id, @sub.status, 'change', true)
                 expect(@sub.value).to eq('change')
                 expect(@log).to eq(['what what', 'change', 'change'])
+
+                reactor.next_tick {
+                    @status.update(@sub.mod_id, @sub.status, 'next')
+                    expect(@sub.value).to eq('next')
+                    expect(@log).to eq(['what what', 'change', 'change', 'next'])
+                }
             }
         end
 
+        it "should unsubscribe to status when module loaded" do
+            expect(@log).to eq([])
+            expect(@sub.value).to be(nil)
+
+            @reactor.run { |reactor|
+                @status.update(@sub.mod_id, @sub.status, 'what what')
+                expect(@sub.value).to eq('what what')
+                expect(@log).to eq(['what what'])
+
+                @controller.loaded[@sub.mod_id] = @mod
+                @status.unsubscribe(@sub)
+
+                @status.update(@sub.mod_id, @sub.status, 'change')
+                expect(@sub.value).to eq('what what')
+                expect(@log).to eq(['what what'])
+
+                @status.update(@sub.mod_id, @sub.status, 'change', true)
+                expect(@sub.value).to eq('what what')
+                expect(@log).to eq(['what what'])
+            }
+        end
+
+        it "should unsubscribe to status when module not loaded" do
+            expect(@log).to eq([])
+            expect(@sub.value).to be(nil)
+
+            @reactor.run { |reactor|
+                @status.update(@sub.mod_id, @sub.status, 'what what')
+                expect(@sub.value).to eq('what what')
+                expect(@log).to eq(['what what'])
+
+                @status.unsubscribe(@sub)
+
+                @status.update(@sub.mod_id, @sub.status, 'change')
+                expect(@sub.value).to eq('what what')
+                expect(@log).to eq(['what what'])
+            }
+        end
+
+        it "should unsubscribe when buried in a promise response" do
+            expect(@log).to eq([])
+            expect(@sub.value).to be(nil)
+
+            @reactor.run { |reactor|
+                @status.update(@sub.mod_id, @sub.status, 'what what')
+                expect(@sub.value).to eq('what what')
+                expect(@log).to eq(['what what'])
+
+                pro_sub = ::Libuv::Q::ResolvedPromise.new(reactor, @sub)
+                @status.unsubscribe(pro_sub)
+
+                reactor.next_tick {
+                    @status.update(@sub.mod_id, @sub.status, 'change')
+                    expect(@sub.value).to eq('what what')
+                    expect(@log).to eq(['what what'])
+                }
+            }
+        end
+    end
+
+
+    describe 'indirect module subscriptions' do
+        before :each do
+            @sys = MockSysProxy.new
+            @sys.add_module(@mod, :Display)
+            @sub = @status.subscribe(
+                status: :testing,
+                callback: proc { |sub| @log << sub.value },
+                on_thread: reactor,
+
+                sys_id: @sys.id,
+                sys_name: "Some System",
+                mod_name: "Display",
+                index: 1
+            )
+        end
+
+        it "should subscribe to module status indirectly" do
+            expect(@log).to eq([])
+            expect(@sub.value).to be(nil)
+
+            @reactor.run { |reactor|
+                @status.reloaded_system(@sys.id, @sys)
+
+                @status.update(@sub.mod_id, @sub.status, 'what what')
+                expect(@sub.value).to eq('what what')
+                expect(@log).to eq(['what what'])
+
+                @status.update(@sub.mod_id, @sub.status, 'change')
+                expect(@sub.value).to eq('change')
+                expect(@log).to eq(['what what', 'change'])
+
+                @status.update(@sub.mod_id, @sub.status, 'change', true)
+                expect(@sub.value).to eq('change')
+                expect(@log).to eq(['what what', 'change', 'change'])
+
+                reactor.next_tick {
+                    @status.update(@sub.mod_id, @sub.status, 'next')
+                    expect(@sub.value).to eq('next')
+                }
+            }
+
+            expect(@log).to eq(['what what', 'change', 'change', 'next'])
+        end
     end
 end
