032324763f7fca8a51e3e6990505b6820a009c31
Stephen von Takach <steve@cotag.me>
2017-01-04 12:25:12 +1100

    (command queue) ensure queues are reset when cancelled

--- lib/orchestrator/device/command_queue.rb
+++ lib/orchestrator/device/command_queue.rb
@@ -102,7 +102,7 @@ module Orchestrator
 
                     while length > 0
                         cmd = @pending_commands.pop
-                        if cmd.class == Symbol
+                        if cmd.is_a? Symbol
                             res = @named_commands[cmd][0]
                             pri = res.shift
                             res << pri
@@ -120,7 +120,7 @@ module Orchestrator
             def cancel_all(msg)
                 while length > 0
                     cmd = @pending_commands.pop
-                    if cmd.class == Symbol
+                    if cmd.is_a? Symbol
                         res = @named_commands[cmd]
                         if res
                             res[1][:defer].reject(msg)
@@ -130,6 +130,9 @@ module Orchestrator
                         cmd[:defer].reject(msg)
                     end
                 end
+            ensure
+                @named_commands = {}
+                @pending_commands = Containers::Heap.new(&@comparison)
                 pop nil
             end
 
@@ -145,7 +148,7 @@ module Orchestrator
                 if @callback && length > 0 && !@waiting
                     next_cmd = @pending_commands.pop
 
-                    if next_cmd.class == Symbol # (named command)
+                    if next_cmd.is_a? Symbol # (named command)
                         result = @named_commands[next_cmd]
                         result[0].shift
                         cmd = result[1]
