ad14d29f59fba70e76210dfadc367171e12bb7f7
Stephen von Takach <steve@cotag.me>
2017-05-16 14:29:40 +0930

    (core:websocket) improve JSON object passing

--- lib/orchestrator/websocket_manager.rb
+++ lib/orchestrator/websocket_manager.rb
@@ -509,10 +509,16 @@ module Orchestrator
 
         def prepare_json(object)
             case object
-            when nil, true, false, Hash, String, Integer, Array, Float
+            when nil, true, false, Hash, String, Integer, Array, Float, Symbol
                 object
             else
-                nil
+                if object.respond_to? :to_json
+                    object.to_json
+                elsif object.respond_to? :to_s
+                    object.to_s
+                else
+                    nil
+                end
             end
         end
     end
