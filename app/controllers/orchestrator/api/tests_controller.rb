a31a4bdbf9d3e9140507091b0ffe20617ea28cf6
Stephen von Takach <steve@cotag.me>
2017-10-17 16:59:29 +1100

    (API:Tests) provide a link to protocol documentation
    Assuming the documentation is linked to in either the spec or driver
    file.

--- app/controllers/orchestrator/api/tests_controller.rb
+++ app/controllers/orchestrator/api/tests_controller.rb
@@ -30,26 +30,57 @@ if Rails.env.development?
                     spec_file = params.permit(:id)[:id]
 
                     begin
+                        # Work out the driver class being tested from the spec file
                         text = File.read("#{spec_file}.rb")
                         klass_name = text.match(/.mock_device\s*\({0,1}\s*\'{0,1}\"{0,1}([^"|^']+)/)[1]
                         file_path = "#{klass_name.underscore}.rb"
 
+                        # Find where the driver file is located
                         path = ::Rails.application.config.orchestrator.module_paths.select { |dir|
                             File.file?(File.join(dir, file_path))
                         }[0]
                         mod_file = File.join(path, file_path)
 
+                        # Load the driver file (reloads over anything previously loaded)
                         load mod_file
                         klass = klass_name.constantize
 
+                        # Attempt to extract a documentation link from the spec file
+                        docs = text.match(/\#\s*(Documentation|Protocol):?\s+(\S*)\s+/i)&.[](2)
+                        docs_uri = nil
+                        if docs
+                            begin
+                                URI.parse(docs)
+                                docs_uri = docs
+                            rescue URI::InvalidURIError
+                            end
+                        end
+
+                        # If no documentation link was found then it checks the driver file
+                        if docs_uri.nil?
+                            text = File.read(mod_file)
+                            docs = text.match(/\#\s*(Documentation|Protocol):?\s+(\S*)\s+/i)&.[](2)
+
+                            if docs
+                                begin
+                                    URI.parse(docs)
+                                    docs_uri = docs
+                                rescue URI::InvalidURIError
+                                end
+                            end
+                        end
+
+                        # Return the details to the browser
                         if klass.respond_to? :__discovery_details
                             render json: {
                                 klass: klass_name,
-                                details: klass.__discovery_details
+                                details: klass.__discovery_details,
+                                documentation: docs_uri
                             }
                         else
                             render json: {
-                                klass: klass_name
+                                klass: klass_name,
+                                documentation: docs_uri
                             }
                         end
                     rescue Exception => e
